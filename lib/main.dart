2e8da49c411ecf2e897774a4f20db7e023a803af
Navil Talukdar <naviltalukdar3@gmail.com>
2023-11-25 02:22:19 -0500

    Nearly complete with a few fixes needed

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(GestureApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class GestureApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+  return MaterialApp(
+    home: GestureAppWidget(),
+  );
+  }
+}
+class GestureAppWidget extends StatefulWidget{
+  @override
+  _GestureAppWidgetState createState() => _GestureAppWidgetState();
+}
+
+class _GestureAppWidgetState extends State<GestureAppWidget>{
+  int gestureNum = 0;
+  List<String> gestures = ['images/Hand high five.png', 'images/Hand Fist Bump.png', 'images/Hand down low.jpeg','images/Hand shake hand.jpeg'];
+
+  void highFive(){
+    setState((){
+      gestureNum = 0;
+    }
+    );
+  }
+
+  void fistBump(){
+    setState((){
+      gestureNum = 1;
+    }
+    );
+  }
+
+  void downLow(){
+    setState((){
+      gestureNum = 2;
+    }
+    );
+  }
+
+  void shakeHand(){
+    setState((){
+      gestureNum = 3;
+    }
+    );
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("The Hand"),
-          elevation: 100,
-          backgroundColor: Color.fromARGB(255, 245, 163, 0),
-        ),
-        body: Center(
-          child: Image(image: AssetImage('images/Hand high five.png'))
-        ),
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hand Gestures'),
       ),
+      body: GestureDetector(
+        onTap: highFive,
+        onLongPress: fistBump,
+        onDoubleTap: shakeHand,
+        onVerticalDragDown: (details) => downLow(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              gestures[gestureNum],
+              height: 200,
+              width: 200
+          ),
+          SizedBox(height:25),
+          Text('Interact with the hand. Tap to high five. Long Press to Fist Bump. Double tap to shake hands. Swipe down for a down low.'),
+          ]
+          )
+      )
     );
   }
 }
